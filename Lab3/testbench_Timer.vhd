aef62ab20cc75d5493acba6236172dbbf101effa
plen276 <69018605+plen276@users.noreply.github.com>
2022-05-11 17:54:14 +1200

    resources

--- Lab3/testbench_Timer.vhd
+++ Lab3/testbench_Timer.vhd
@@ -12,16 +12,16 @@ architecture my_test of test_Timer is
 	signal t_Data_in : std_logic_vector(9 downto 0);
   signal t_Time_out : std_logic;
   -- Comment out the next 2 lines for running on board.
-  signal t_OSeg0, t_OSeg1, t_OSeg2 : std_logic_vector(7 downto 0);
-  signal t_Tens_seconds : std_logic_vector(3 downto 0);
+--   signal t_OSeg0, t_OSeg1, t_OSeg2 : std_logic_vector(7 downto 0);
+--   signal t_Tens_seconds : std_logic_vector(3 downto 0);
 
 	component Timer is
 		port (Clock, Start : in std_logic;
 		      Data_in : in std_logic_vector(9 downto 0);
 		      Time_out : out std_logic;
 		      -- Comment out the next 2 lines for running on board.
-		      OSeg0, OSeg1, OSeg2 : out std_logic_vector(7 downto 0);
-		      OTens_seconds : out std_logic_vector(3 downto 0));
+		    --   OSeg0, OSeg1, OSeg2 : out std_logic_vector(7 downto 0);
+		    --   OTens_seconds : out std_logic_vector(3 downto 0));
 	end component Timer;
 begin
   DUT: Timer 
@@ -29,7 +29,7 @@ begin
 	-- Initialization process (code that executes only once). 
 	init: process
 	begin 
-	  t_Start <= '1', '0' after 2000 ms, '1' after 3000 ms;
+	  t_Start <= '0', '1' after 50 ns;
 	  t_Data_in <= "1001001001";
 		wait;
 		
@@ -38,9 +38,9 @@ begin
 	-- clock generation 
 	clk_gen: process 
 	begin 
-		wait for 500 ms; 
+		wait for 5 ns; 
 		t_Clock <= '1'; 
-		wait for 500 ms; 
+		wait for 5 ns; 
 		t_Clock <= '0';
 	end process clk_gen;
 end architecture my_test;
